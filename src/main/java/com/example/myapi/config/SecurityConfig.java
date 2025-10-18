5268d9faee92111e117b5f6f5526cc4edebd2304
jesus <usajesuscash@gmail.com>
2025-10-18 13:50:02 -0500

    MODIFICACINES

--- src/main/java/com/example/myapi/config/SecurityConfig.java
+++ src/main/java/com/example/myapi/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 @Configuration
 public class SecurityConfig {
 
+    @SuppressWarnings("removal")
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
--- src/main/java/com/example/myapi/model/entity/Product.java
+++ src/main/java/com/example/myapi/model/entity/Product.java
@@ -20,7 +20,7 @@ public class Product {
 
     private String name;
 
-    private String description;
+    private String description; //string
 
     private String feature;
 
